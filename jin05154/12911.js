c746b9a1aac985e4c6172cd0f0d2a74e57f9c7b6
yuminlee <dorayuminlee@gmail.com>
2022-07-30 21:48:05 +0900

    [PGS] 다음 큰 숫자 / Lv.2 / 코드 수정
    https://school.programmers.co.kr/learn/courses/30/lessons/12911

--- jin05154/12911.js
+++ jin05154/12911.js
@@ -3,14 +3,6 @@ function convertBinary(n) {
   return convertBinary(Math.floor(n / 2)) + String(n % 2);
 }
 
-function convertDecimal(binary) {
-  let decimal = 0;
-  [...binary].forEach((bi, i) => {
-    decimal += bi * Math.pow(2, binary.length - i - 1);
-  });
-  return decimal;
-}
-
 function solution(n) {
   const binaryN = convertBinary(n);
   const countOne = binaryN.split("1").length;
@@ -26,10 +18,3 @@ function solution(n) {
 
   return answer;
 }
-
-// 처음엔 문제 입출력 예시 따라서 경우 나눠서 계산해야하는 줄 알았음
-// if (!binaryN.includes("0")) {
-//   miniMax = "10" + binaryN.slice(1);
-//   answer = convertDecimal(miniMax);
-// } else {
-// }
